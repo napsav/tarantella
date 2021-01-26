b2dd3c3a4e97fd5fc75a982d6ccafaac084a88c5
napsav <savi.napolitano@gmail.com>
2021-01-26 17:17:20 +0100

    let's go

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.PORT || 5000
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({
     extended: false
@@ -10,34 +10,57 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 var titoli = [];
-
+let partecipanti = 0;
+let ip = [];
 // Index
 app.get('/', (req, res) => {
-    res.render('index')
+    let alert = null;
+    let done = false;
+    if (ip.includes(req.ip)) {
+        done = true;
+        alert = "Hai già inserito un titolo!"
+    }else if (titoli.length === partecipanti) {
+        done = true;
+        alert = "Titoli al completo"
+    }
+    res.render('index', {color: "#ff6161", done, alert})
 })
 
 app.post('/', (req, res) => {
     var done = false;
     var alert = null;
+    var color = "#ff6161"
     var titolo = req.body.titolo;
+    console.log(titoli.length)
     if (titolo === null || titolo === "") {
         alert = "Il titolo non può essere vuoto!";
-    } else {
-        alert = "Titolo inviato con successo";
+    } else if (titoli.length === partecipanti) {
+        done = true;
+        alert = "Titoli al completo"
+    } else if (ip.includes(req.ip)) {
+        done = true;
+        alert = "Hai già inserito un titolo!"
+    } else if (titoli.length + 1 <= partecipanti) {
+        alert = "Titolo inviato con successo, puoi chiudere";
+        color = "green"
         done = true;
         titoli.push(titolo.trim());
+        ip.push(req.ip)
+        console.log(ip)
+    } else {
+        alert="Errore del server e Saverio è un coglione"
     }
     res.render('index.pug',
-        { alert: alert, done: done })
+        { color: color, alert: alert, done: done })
 })
 
 
 app.get('/partecipante', (req, res) => {
     var alert = null;
     var titoloscelto = null;
-    if (titoli.length < 3) {
+    if (titoli.length < partecipanti) {
         alert = "Non tutti hanno inviato il titolo, devi aspettare! (Se lo hanno inviato, prova a ricaricare la pagina)"
-    } else if (titoli.length > 3) {
+    } else if (titoli.length > partecipanti) {
         alert = "Qualche deficiente ha messo più di un titolo."
     } else {
         titoloscelto = titoli[Math.floor(Math.random() * titoli.length)];
@@ -48,15 +71,35 @@ app.get('/partecipante', (req, res) => {
     })
 })
 
-app.get('/reset', (req, res) => {
+app.get('/admin', (req, res) => {
     res.render('reset.pug',
-        { n: titoli.length })
+        { n: titoli.length, part: partecipanti })
 })
 
 app.post('/reset', (req, res) => {
     const pass = process.env.PASSWORD;
     if (req.body.password === pass) {
         titoli = [];
+        res.send("Fatto")
+    } else {
+        res.send("Non autorizzato");
+    }
+})
+
+app.post('/cambiopartecipanti', (req, res) => {
+    const pass = process.env.PASSWORD;
+    if (req.body.password === pass) {
+        partecipanti = req.body.partecipanti;
+        res.send("Fatto")
+    } else {
+        res.send("Non autorizzato");
+    }
+})
+
+app.post('/resetip', (req, res) => {
+    const pass = process.env.PASSWORD;
+    if (req.body.password === pass) {
+        ip = [];
         res.send("Fatto")
     } else {
         res.send("Non autorizzato");
